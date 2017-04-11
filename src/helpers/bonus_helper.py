115dfc694459b6646ec18491de8eb62d8cd594d1
Tobias Schmidt <royalts@gmail.com>
2017-04-11 13:00:49 -0700

    scale linear bonus scheme so that the full bonus is paid on full agreement with other Turkers

--- src/helpers/bonus_helper.py
+++ src/helpers/bonus_helper.py
@@ -26,8 +26,8 @@ class LinearBonusType(BonusType) :
     @staticmethod
     def calculate_bonus(bonus_info, agreed, total) :
         consenters = max(0.0, agreed - 1.0)
-        amt = (1.0 + bonus_info['bonusmultiplier']) * (consenters / total)
-        exp = '%s points for agreeing with %d of %d workers on a question with linear payment and a bonus multiplier of %s' % (amt, consenters, total, (1 + bonus_info['bonusmultiplier']))
+        amt = (1.0 + bonus_info['bonusmultiplier']) * (consenters / total) * (total / (total - 1))
+        exp = '%s points for agreeing with %d of the %d other workers on a question with linear payment and a bonus multiplier of %s' % (amt, consenters, total - 1, (1 + bonus_info['bonusmultiplier']))
         return (amt, exp)
 
 class ThreholdBonusType(BonusType) :
