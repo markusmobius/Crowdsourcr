cef93ed797a4f420b34e8b8e9fbba2cb5c6b7e6b
Adina Bechhofer <33551501+AdinaBechhofer@users.noreply.github.com>
2020-07-20 13:49:14 -0400

    jaccard added

--- src/helpers/jaccard_machine.py
+++ src/helpers/jaccard_machine.py
@@ -1,8 +1,30 @@
+from spacy.lang.en import English
 
 class Jaccard:
-  
+    nlp = English()
+
     def getTokens(self,text):
-        return {"good":1, "stuff":1}
+        """ takes a string, removes periods and commas and return a dictionary that maps each token 
+        in the string to its frequency in the text"""
+        doc = nlp(text.replace(".", "").replace(",", ""))
+        tokens = {}
+        for token in doc:
+            if token in tokens:
+                tokens[token] +=1
+            else:
+                tokens[token] =1
+        return tokens
 
     def compare(self, tokens1,tokens2):
-        return 0.5
+        ''' Inputs: tokens1 tokens2, two word bags
+        Output: a float, the Jaccard similarity value'''
+        overlap = 0
+        for key in tokens1:
+            if key in tokens2:
+                if tokens2[key] > tokens1[key]:
+                    overlap += tokens1[key]
+                else:
+                    overlap += tokens2[key]
+        tokens1_size = sum(tokens1.values())
+        tokens2_size = sum(tokens2.values())
+        return 1.0*overlap / (tokens1_size + tokens2_size - overlap)
